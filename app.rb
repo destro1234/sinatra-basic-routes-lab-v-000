7a55e091d61fe73e7ab78cfdb5f829440365fac1
Elias Ventura <eliasventura23@gmail.com>
2019-02-26 18:56:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/name' do
-    "My name is __"
+    "My name is Elias."
   end
+
+  get '/hometown' do
+    "My hometown is NYC."
+  end
+
+  get
+
+
 end
